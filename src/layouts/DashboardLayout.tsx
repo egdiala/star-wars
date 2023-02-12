8dcda960899f942295dcbd7bb86831712c1af7ed
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-13 00:45:38 +0100

    fix: adjust padding on dashboard layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -8,7 +8,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
             <Sidebar />
             <main className="flex-1 overflow-hidden relative">
                 <Navbar />
-                <div className="pt-24 lg:pt-28 px-5 lg:px-6 overflow-auto flex flex-col justify-between h-80 min-h-full mx-auto">
+                <div className="pt-28 px-10 overflow-auto flex flex-col justify-between h-80 min-h-full mx-auto">
                     {children}
                 </div>
             </main>
