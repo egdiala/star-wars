189820b021f146eb0c9f6d6c1b1adab8dc595263
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-13 00:10:10 +0100

    feat: restyle navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ import ellipsisHorizontalSolid from '@iconify/icons-heroicons/ellipsis-horizonta
 import bellIcon from '@iconify/icons-heroicons/bell';
 
 export const Navbar = () => {
-    return <header className="absolute top-0 left-0 right-0 z-10 shadow-md bg-white">
+    return <header className="absolute top-0 left-0 right-0 z-10 shadow-sm bg-white">
         <div className="flex flex-grow items-center py-5 px-10">
-            <div className="flex flex-wrap gap-9 w-fit items-center justify-end ml-auto">
+            <div className="flex flex-wrap gap-6 w-fit items-center justify-end ml-auto">
                 <Icon icon={bellIcon} className='text-grey-700 font-bold text-2xl' />
                 <hr className='w-6 rotate-90 border-grey-200' />
                 <div className="flex items-center gap-6">
