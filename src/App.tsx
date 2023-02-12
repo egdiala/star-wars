c1395204750a3f923691f9be0915b11ae9467cb3
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-13 00:09:19 +0100

    fix: remove main element

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { NotFound } from './pages/NotFound'
 import { Dashboard } from './pages/Dashboard'
+import './App.css'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <main className='relative'>
         <Routes>
           <Route path='/' element={<Login />} errorElement={<NotFound />} />
           <Route path='dashboard' element={<Dashboard />} errorElement={<NotFound />} />
         </Routes>
-      </main>
     </BrowserRouter>
   )
 }
