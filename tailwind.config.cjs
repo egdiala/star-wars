e1bdbbbabcb403ab72105b2c3582658f45bf123c
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-12 15:01:09 +0100

    feat: setup project with vite, react-ts, router and tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: {
+        10: '#A9C1FF',
+        100: '#0A74DC',
+        200: '#031434'
+      },
+      grey: {
+        10: '#A4A7B7',
+        100: '#B0B9C8',
+        200: '#E5E5E5',
+        300: '#C4C4C4',
+        400: '#303B54',
+        500: '#434854'
+      },
+      success: {
+        100: '#00992B',
+        50: '#A9FFE0'
+      },
+      error: '#FFA9EC',
+      warning: '#FDFFA9',
+      dark: '#0B2253',
+      white: '#FFFFFF',
+      black: '#000000',
+      transparent: 'transparent'
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
