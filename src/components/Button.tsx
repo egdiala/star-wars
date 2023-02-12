6765a5c49b745c1ba60119d2af1a68031b4fbd6f
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-12 18:19:23 +0100

    feat: bind attributes to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-    return <button onClick={props.onClick} className="bg-primary-100 text-white py-3 w-full rounded-md">
+    return <button {...props} onClick={props.onClick} className="bg-primary-100 text-white py-3 w-full rounded-md disabled:cursor-not-allowed">
         {props.label}
     </button>
 }
