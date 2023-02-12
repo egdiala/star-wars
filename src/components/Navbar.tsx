bea09396f4dcb560bc3e7d188f1d78a6e054b112
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-12 20:27:38 +0100

    feat: create Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Icon } from '@iconify/react';
+import ellipsisHorizontalSolid from '@iconify/icons-heroicons/ellipsis-horizontal-solid';
+import bellIcon from '@iconify/icons-heroicons/bell';
+
+export const Navbar = () => {
+    return <header className="absolute top-0 left-0 right-0 z-10 shadow-md bg-white">
+        <div className="flex flex-grow items-center py-5 px-10">
+            <div className="flex flex-wrap gap-9 w-fit items-center justify-end ml-auto">
+                <Icon icon={bellIcon} className='text-grey-700 font-bold text-2xl' />
+                <hr className='w-6 rotate-90 border-grey-200' />
+                <div className="flex items-center gap-6">
+                    <img src='/assets/account.svg'/>
+                    <span className="font-normal text-base text-grey-400">John Doe</span>
+                </div>
+                <Icon icon={ellipsisHorizontalSolid} className='text-grey-300 font-bold text-2xl' />
+            </div>
+        </div>
+    </header>
+}
