3d960f3d8230f0a4ea8a006443896b05a8f874c9
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-12 15:03:00 +0100

    feat: create Login Page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+export const Login = () => {
+    return <>
+        <section className="h-full w-full">
+            <div className="grid grid-cols-1 lg:grid-cols-3 h-full">
+                <div className="hidden lg:grid content-center justify-items-center px-12 bg-primary-200">
+                    <img src="/assets/star-wars.png" className="w-full" />
+                </div>
+                <div className="col-span-2 grid w-full min-h-screen bg-white">
+                    <div className="border "></div>
+                </div>
+            </div>
+        </section>
+    </>
+}
