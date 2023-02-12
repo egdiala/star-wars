43a88ecc0564e89326b45c90e125658b1a5ebac3
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-13 00:11:05 +0100

    feat: add Sidebar component to DashboardLayout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Navbar } from "../components/Navbar";
+import { Sidebar } from "../components/Sidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <>
-            <Navbar />
-            {children}
+            <Sidebar />
+            <main className="flex-1 overflow-hidden relative">
+                <Navbar />
+                <div className="pt-24 lg:pt-28 px-5 lg:px-6 overflow-auto flex flex-col justify-between h-80 min-h-full mx-auto">
+                    {children}
+                </div>
+            </main>
         </>
     )
 }
