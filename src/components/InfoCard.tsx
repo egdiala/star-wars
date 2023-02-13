fce9e933ee001282cca512a431888483d179699f
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2023-02-13 14:07:07 +0100

    fix: fix styling on cards

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -7,7 +7,7 @@ export interface InfoCardProps {
 
 export const InfoCard: React.FC<InfoCardProps> = (props) => {
     return <>
-        <div className="grid gap-6 px-5 py-6 bg-white drop-shadow-lg rounded-xl">
+        <div className="grid gap-6 h-fit px-5 py-6 bg-white drop-shadow-lg rounded-xl">
             <div className="flex flex-wrap items-center justify-between gap-28">
                 <h1 className="font-bold text-base text-grey-500">{props.title}</h1>
                 <div className={`w-7 h-7 rounded ${props.color}`} />
